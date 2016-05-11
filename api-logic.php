023cb1fb84b0aa53b5f255c9c1a0456b62d70243
HawiCaesar <brian.odhiambo932@gmail.com>
2016-05-11 17:16:56 +0300

    first commit

---
+++ api-logic.php
@@ -0,0 +1,28 @@
+<?php
+
+//connect to database and fetch data
+
+$mysqli = new mysqli("localhost", "root", "toor1", "ona");
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+if ($result = $mysqli->query("SELECT * from county")) {
+
+    while($row = $result->fetch_assoc()) {
+		$rows[]=$row;
+	}
+
+    $result->close();
+}
+
+print_r($rows);
+
+//fetch ona users via curl
+
+
+
+
+?>

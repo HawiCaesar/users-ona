0a66b5164be5357950d50c9620bc7c5c5d687840
HawiCaesar <brian.odhiambo932@gmail.com>
2016-05-13 11:53:42 +0300

    changed database colum, added gif file, corrected map logic

--- map-api-logic.php
+++ map-api-logic.php
@@ -37,83 +37,90 @@ curl_close($ch);
 
 $decoded_usernames=json_decode($result,true);
 
-$county_count = 0;
-$reminder = 0;
+foreach ($decoded_usernames as $value){
+
+	if($value['first_name']==""){
+		$usersnames[] = $value['username'];
+	}
+
+}
+
+$total_without_first_name = count($usersnames)."<br />";
 
 $data110 = array();
 $data1120 = array();
 $data2030 = array();
 $data3047 = array();
+$data4047 = array();
 
 foreach ($rows as $row) {
-
-	//echo $total_number_users."<br />";
-	$rand = rand(1, 47);
-
-	//echo "random number is ".$rand."<br />";
-
-	// $county_count+=1;
-
-	// echo $total_number_users."-".$rand."=".$reminder = $total_number_users-=$rand."<br />";
-	// echo "___________________________________________".$row['id']."<br />";
-
-	if($rand>=1 && $rand <10){
-		$data110[]= array('value'=>$rand,'code'=>$row['c_code']);
-	}elseif($rand>=10 && $rand <20){
-		$data1120[]= array('value'=>$rand,'code'=>$row['c_code']);
-	}elseif($rand>=20 && $rand <30){
-		$data2030[]= array('value'=>$rand,'code'=>$row['c_code']);
-	}elseif($rand>=30){
-		$data3047[]= array('value'=>$rand,'code'=>$row['c_code']);
+	$map_data = $total_without_first_name-$row['value'];
+	
+	if($map_data>=5000){
+		$data110[]= array('value'=>$map_data,'code'=>$row['c_code']);
+	}elseif($map_data<=4999 && $map_data>=4500){
+		$data1120[]= array('value'=>$map_data,'code'=>$row['c_code']);
+	}elseif($map_data<=4499 && $map_data>=4000){
+		$data2030[]= array('value'=>$map_data,'code'=>$row['c_code']);
+	}elseif($map_data<=3999 && $map_data>=3500){
+		$data3047[]= array('value'=>$map_data,'code'=>$row['c_code']);
+	}elseif($map_data<=3499){
+		$data4047[]= array('value'=>$map_data,'code'=>$row['c_code']);
 	}
 	
 }
 
-echo "<pre>";
-print_r($data110);
-echo "</pre>";
+// echo "<pre>";
+// print_r($data110);
+// echo "</pre>";
 
-echo "<pre>";
-print_r($data1120);
-echo "</pre>";
+// echo "<pre>";
+// print_r($data1120);
+// echo "</pre>";
 
-echo "<pre>";
-print_r($data2030);
-echo "</pre>";
+// echo "<pre>";
+// print_r($data2030);
+// echo "</pre>";
 
-echo "<pre>";
-print_r($data3047);
-echo "</pre>";
+// echo "<pre>";
+// print_r($data3047);
+// echo "</pre>";
 
 //Map stuff
-// $a = array(
-// 			array(
+$a = array(
+			array(
 
-// 				'name' => '>=1 Users <10 Users',
-// 				'color' => '#D54339',
-// 				'data' => $data110
-// 				),
-// 			array(
+				'name' => '> = 5000 Users',
+				'color' => '#D54339',
+				'data' => $data110
+				),
+			array(
 
-// 				'name' => '>=10 Users <20 Users',
-// 				'color' => '#495136',
-// 				'data' => $data1120
-// 				),
-// 			array(
+				'name' => '< = 4999 - > = 4500 Users',
+				'color' => '#495136',
+				'data' => $data1120
+				),
+			array(
 
-// 				'name' => '>= 20 Users < 30 Users',
-// 				'color' => '#2372b2',
-// 				'data' => $data2030
-// 				),
-// 			array(
+				'name' => '< = 4499 - 4000 Users',
+				'color' => '#2372b2',
+				'data' => $data2030
+				),
+			array(
 
-// 				'name' => '>=30 Users',
-// 				'color' => '#c65b08',
-// 				'data' => $data3047
-// 				)
-// 		);
+				'name' => '< = 3499 - 3500 Users',
+				'color' => '#c65b08',
+				'data' => $data3047
+				),
+			array(
 
-// echo json_encode($a);
+				'name' => '< = 3499',
+				'color' => '#69827f',
+				'data' => $data4047
+				)
+		);
+
+ echo json_encode($a);
 
 
 ?>

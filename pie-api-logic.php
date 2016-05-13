c59bbef130a8ade5e87d30e982736a07a4b691d1
HawiCaesar <brian.odhiambo932@gmail.com>
2016-05-13 12:09:12 +0300

    removed some unnecessary database logic

--- pie-api-logic.php
+++ pie-api-logic.php
@@ -1,27 +1,5 @@
 <?php
 
-//connect to database and fetch data
-
-$mysqli = new mysqli("localhost", "root", "toor1", "ona");
-
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-if ($result = $mysqli->query("SELECT * from county")) {
-
-    while($row = $result->fetch_assoc()) {
-		$rows[]=$row;
-	}
-
-    $result->close();
-}
-
-//print_r($rows);die;
-
-//fetch ona users via curl
-
 $url = 'https://api.ona.io/api/v1/users';
 
 $ch = curl_init();
